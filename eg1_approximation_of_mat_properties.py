f3a825ec44bb2058f98e345b99b66cd9518b10f6
Shadi Alameddin <shadialameddin@gmail.com>
2022-08-09 15:04:18 +0200

    update the 1st example

--- eg1_approximation_of_mat_properties.py
+++ eg1_approximation_of_mat_properties.py
@@ -10,12 +10,14 @@ temp2 = 1300
 n_tests = 25
 test_temperatures = np.linspace(temp1, temp2, num=n_tests)
 test_alphas = np.linspace(0, 1, num=n_tests)
+n_approaches = 5
+
+# TODO new error indicator
+err_indic = np.zeros((n_tests))
 
 for mat_id in range(2):
     sampling_C = [[stiffness_cu(temp1), stiffness_cu(temp2)], [stiffness_wsc(temp1), stiffness_wsc(temp2)]]
     sampling_eps = [[thermal_strain_cu(temp1), thermal_strain_cu(temp2)], [thermal_strain_wsc(temp1), thermal_strain_wsc(temp2)]]
-
-    n_approaches = 5
     max_eig_value, trace_eps = [np.zeros((n_approaches, n_tests)) for _ in range(2)]
 
     for idx, alpha in enumerate(test_alphas):
@@ -62,6 +64,17 @@ for mat_id in range(2):
         trace_eps[3, idx] = opt2_trace_eps
         trace_eps[4, idx] = opt4_trace_eps
 
+        # TODO clean
+        approx_C, approx_eps = naive(alpha, sampling_C, sampling_eps, ref_C, ref_eps)
+        err = lambda x, y: la.norm(x - y) / (la.norm(y) if la.norm(y) > 0 else 1)
+        # err_indic[idx] += err(la.eigvalsh(approx_C[mat_id]), la.eigvalsh(ref_C[mat_id])) + err(opt4_trace_eps, ref_trace_eps)
+        # err_indic[idx] += err(opt4_max_eig_value, ref_max_eig_value) + err(opt4_trace_eps, ref_trace_eps)
+        # err_indic[idx] += err(opt4_max_eig_value, ref_max_eig_value)
+        err_indic[idx] += err(np.sort(la.eigvalsh(approx_C[mat_id])), np.sort(la.eigvalsh(ref_C[mat_id])))
+        # err_indic[idx] += err(naive_trace_eps, ref_trace_eps)
+        # err_indic[idx] += 0.7 * err(np.sort(la.eigvalsh(approx_C[mat_id])), np.sort(la.eigvalsh(ref_C[mat_id]))) + 0.3 * err(
+        #     naive_trace_eps, ref_trace_eps)
+
     max_eig_value /= np.max(max_eig_value)
     trace_eps /= np.max(trace_eps)
 
@@ -85,3 +98,10 @@ for mat_id in range(2):
     for idx in range(n_approaches):
         plt.plot(test_temperatures, trace_eps[idx], label=labels[idx], marker=markers[idx], color=colors[idx])
     plot_and_save(xlabel, ylabel, fig_name, [temp1, temp2], [0, 1])
+
+fig_name = f'eg1_cumulative_error_indicator'
+xlabel = 'Temperature [K]'
+ylabel = r'Cumulative error indicator [-]'
+plt.figure(figsize=(6 * cm, 6 * cm), dpi=600)
+plt.plot(test_temperatures, err_indic, label=labels[0], marker=markers[0], color=colors[0])
+plot_and_save(xlabel, ylabel, fig_name, [temp1, temp2], [0, None])
